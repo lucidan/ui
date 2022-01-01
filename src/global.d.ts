a9d73314ccd7a67a17abf5b2c31b720b871edd5b
Puru Vijay <awesomepuruvj@gmail.com>
2022-01-01 15:25:54 +0530

    Minor stuff

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,7 +1,33 @@
 /// <reference types="@sveltejs/kit" />
 
-declare namespace svelte.JSX {
-	interface DomAttributes {
-		onrectchange?: (e: CustomEvent<PRect>) => void;
-	}
+/// <reference types="svelte" />
+/// <reference types="vite/client" />
+/// <reference types="vite-plugin-pwa/client" />
+/// <reference types="unplugin-icons/types/svelte" />
+
+type ObjectKeys<Obj> = Obj extends object
+	? (keyof Obj)[]
+	: Obj extends number
+	? []
+	: Obj extends Array<any> | string
+	? string[]
+	: never;
+
+interface ObjectConstructor {
+	keys<ObjectType>(o: ObjectType): ObjectKeys<ObjectType>;
+	entries<ObjType>(o: ObjType): [Unpacked<ObjectKeys<ObjType>>, ObjType[keyof ObjType]][];
+}
+
+interface Storage {
+	getItem<T extends string>(key: string): T | null;
+}
+
+type Unpacked<ArrayLike> = ArrayLike extends (infer RootType)[] ? RootType : ArrayLike;
+
+type Unpromisify<PromiseLike> = PromiseLike extends Promise<infer RootType>
+	? RootType
+	: PromiseLike;
+
+interface Array<T> {
+	fill<T extends any>(value: T, start?: number | undefined, end?: number | undefined): T[];
 }
---
+++ src/lib/alert/Alert.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import { waitFor } from '../utils/wait-for';
+	import { VisuallyHidden } from '../visually-hidden';
+
+	let politeRegion: HTMLDivElement;
+	let assertiveRegion: HTMLDivElement;
+</script>
+
+<!-- {#await renderTimerPromise}
+	{#each Object.entries(liveRegions) as [regionType, element]}
+		{#if element}
+			<VisuallyHidden>
+				<div role={regionType === 'assertive' ? 'alert' : 'status'} aria-live={regionType}>
+					
+				</div>
+			</VisuallyHidden>
+		{/if}
+	{/each}
+{/await} -->
+
+<!-- Regions -->
+<VisuallyHidden>
+	<div data-reach-live-polite="true" bind:this={politeRegion} />
+	<div data-reach-live-assertive="true" bind:this={assertiveRegion} />
+</VisuallyHidden>
+
+{#await waitFor(500) then _}
+	<VisuallyHidden />
+{/await}
