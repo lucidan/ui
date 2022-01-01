e36ea042a37a3c544f55997bb0b851004af569db
Puru Vijay <awesomepuruvj@gmail.com>
2022-01-01 19:54:47 +0530

    Delete events.ts

--- src/lib/utils/types.d.ts
+++ src/lib/utils/types.d.ts
@@ -1,7 +1,5 @@
 export type Maybe<T> = T | undefined;
 
-export type SaralEvent<T extends object> = CustomEvent<T & { cancel: () => void }>;
-
 export type ElementProps<T extends HTMLElement = HTMLDivElement> = Omit<
 	svelte.JSX.HTMLAttributes<T>,
 	keyof svelte.JSX.DOMAttributes<T>
