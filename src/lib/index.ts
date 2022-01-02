9f210872c026471128f18359d00a9f1341602d1b
Puru Vijay <awesomepuruvj@gmail.com>
2022-01-02 13:23:46 +0530

    Basic dialog shell

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,2 +1,6 @@
-export * from './slider';
+export * from './alert';
+export * from './portal';
 export * from './rect';
+export * from './slider';
+export * from './visually-hidden';
+export * from './dialog';
